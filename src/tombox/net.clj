29293e3afb8c0a5f4d534ef0a858e3f2ea21f71d
tom <ssisksl77@gmail.com>
2017-03-24 01:10:49 +0900

    자바 라이브러리를 사용해서 MX-RECORDS 확인. 메일 유효성 체크를 위함. (mxlookup java interop on clojure)

--- src/tombox/net.clj
+++ src/tombox/net.clj
@@ -1,3 +1,23 @@
 (ns tombox.net
-  :use '[clojure.java.shell :only [sh]])
+  (:use [clojure.java.shell :only [sh]])
+  (:import [javax.naming]
+           [javax.naming.directory InitialDirContext]))
 
+(defn nslookup
+  ([hostname] (sh "nslookup" hostname))
+  ([hostname query] (sh "nslookup" query  hostname)))
+
+(defn nslookup-mx [hostname]
+  (:out (ns-lookup "-query=mx" hostname)))
+
+(defn smtp-mxlookup [hostname]
+  (let [env (java.util.Hashtable. {"java.naming.factory.initial" "com.sun.jndi.dns.DnsContextFactory"})]
+    (enumeration-seq (. (. (. (InitialDirContext. env) getAttributes hostname (into-array String ["MX"])) get "MX") getAll))))
+
+
+(defn mx-isexist? [mxlookup-result]
+  (if-let [result mxlookup-result]
+    (not (empty? result))))
+
+;;example
+;; (mx-isexist? (smtp-mxlookup "google.com"))
